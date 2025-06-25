9ce5743e5d12b0bb07eeb20ff98602b1f8e80579
Khaled.Elshamy <khaled.elshamy@geidea.net>
2025-06-25 14:08:24 +0300

    first commit

---
+++ Telda-MoviesAppTests/Telda_MoviesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Telda_MoviesAppTests.swift
+//  Telda-MoviesAppTests
+//
+//  Created by Khaled Elshamy on 25/06/2025.
+//
+
+import Testing
+@testable import Telda_MoviesApp
+
+struct Telda_MoviesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
