2f8f9066ffceb70d79484f8f2cf9c976fd34ced4
Khaled.Elshamy <khaled.elshamy@geidea.net>
2025-06-27 18:09:54 +0300

    implement the main functions needed for MoviesListViewModel

--- Telda-MoviesApp/Presentations/MoviesScene/MovisList/ViewModel/MoviewListViewModel.swift
+++ Telda-MoviesApp/Presentations/MoviesScene/MovisList/ViewModel/MoviewListViewModel.swift
@@ -13,39 +13,143 @@ struct MoviesListViewModelActions {
 }
 
 protocol MoviesListViewModelInput {
-//    func viewDidLoad()
-//    func didLoadNextPage()
-//    func didSearch(query: String)
-//    func didCancelSearch()
+    func didLoadNextPage()
+    func didSearch(query: String)
+    func didCancelSearch()
 //    func showQueriesSuggestions()
 //    func closeQueriesSuggestions()
-//    func didSelectItem(at index: Int)
+    func didSelectItem(at index: Int)
+}
+enum MoviesListViewModelLoading {
+    case fullScreen
+    case nextPage
 }
 
 protocol MoviesListViewModelOutput {
-//    var items: Observable<[MoviesListItemViewModel]> { get } /// Also we can calculate view model items on demand:  https://github.com/kudoleh/iOS-Clean-Architecture-MVVM/pull/10/files
-//    var loading: Observable<MoviesListViewModelLoading?> { get }
-//    var query: Observable<String> { get }
-//    var error: Observable<String> { get }
-//    var isEmpty: Bool { get }
-//    var screenTitle: String { get }
-//    var emptyDataTitle: String { get }
-//    var errorTitle: String { get }
-//    var searchBarPlaceholder: String { get }
+    var items: Observable<[MoviesListItemViewModel]> { get } /// Also we can
+    var loading: Observable<MoviesListViewModelLoading?> { get }
+    var query: Observable<String> { get }
+    var error: Observable<String> { get }
+    var isEmpty: Bool { get }
+    var screenTitle: String { get }
+    var emptyDataTitle: String { get }
+    var errorTitle: String { get }
+    var searchBarPlaceholder: String { get }
 }
 
 typealias MoviesListViewModel = MoviesListViewModelInput & MoviesListViewModelOutput
 
 
-class DefaultMoviesListViewModel: MoviesListViewModel {
+final class DefaultMoviesListViewModel: MoviesListViewModel {
     
     private let searchMoviesUseCase: SearchMoviesUseCase
+    private let actions: MoviesListViewModelActions?
     
     var currentPage: Int = 0
     var totalPageCount: Int = 1
     var hasMorePages: Bool { currentPage < totalPageCount }
     var nextPage: Int { hasMorePages ? currentPage + 1 : currentPage }
     
+    private var pages: [MoviesPage] = []
     private var moviesLoadTask: Cancellable? { willSet { moviesLoadTask?.cancel() } }
     private let mainQueue: DispatchQueueType
+    
+    
+    let items: Observable<[MoviesListItemViewModel]> = Observable([])
+    let loading: Observable<MoviesListViewModelLoading?> = Observable(.none)
+    let query: Observable<String> = Observable("")
+    let error: Observable<String> = Observable("")
+    var isEmpty: Bool { return items.value.isEmpty }
+    let screenTitle = NSLocalizedString("Movies", comment: "")
+    let emptyDataTitle = NSLocalizedString("Search results", comment: "")
+    let errorTitle = NSLocalizedString("Error", comment: "")
+    let searchBarPlaceholder = NSLocalizedString("Search Movies", comment: "")
+    
+    init(
+        searchMoviesUseCase: SearchMoviesUseCase,
+        actions: MoviesListViewModelActions? = nil,
+        mainQueue: DispatchQueueType = DispatchQueue.main
+    ) {
+        self.searchMoviesUseCase = searchMoviesUseCase
+        self.actions = actions
+        self.mainQueue = mainQueue
+    }
+    
+    private func appendPage(_ moviesPage: MoviesPage) {
+        currentPage = moviesPage.page
+        totalPageCount = moviesPage.totalPages
+
+        pages = pages
+            .filter { $0.page != moviesPage.page }
+            + [moviesPage]
+
+        items.value = pages.movies.map(MoviesListItemViewModel.init)
+    }
+
+    private func resetPages() {
+        currentPage = 0
+        totalPageCount = 1
+        pages.removeAll()
+        items.value.removeAll()
+    }
+
+    private func load(movieQuery: MovieQuery,
+                      loading: MoviesListViewModelLoading){
+        self.loading.value = loading
+        query.value = movieQuery.query
+
+        moviesLoadTask = searchMoviesUseCase.execute(
+            requestValue: .init(query: movieQuery, page: nextPage),
+            cached: { [weak self] page in
+                self?.mainQueue.async {
+                    self?.appendPage(page)
+                }
+            },
+            completion: { [weak self] result in
+                self?.mainQueue.async {
+                    switch result {
+                    case .success(let page):
+                        self?.appendPage(page)
+                    case .failure(let error):
+                        self?.handle(error: error)
+                    }
+                    self?.loading.value = .none
+                }
+        })
+    }
+
+    private func handle(error: Error) {
+    }
+
+    private func update(movieQuery: MovieQuery) {
+        resetPages()
+        load(movieQuery: movieQuery, loading: .fullScreen)
+    }
+}
+
+// MARK: - Input methods
+
+extension DefaultMoviesListViewModel {
+    func didLoadNextPage() {
+        guard hasMorePages, loading.value == .none else { return }
+        load(movieQuery: .init(query: query.value),
+             loading: .nextPage)
+    }
+    
+    func didSearch(query: String) {
+        guard !query.isEmpty else {return}
+        update(movieQuery: MovieQuery(query: query))
+    }
+    
+    func didCancelSearch() {
+        moviesLoadTask?.cancel()
+    }
+    
+    func didSelectItem(at index: Int) {
+        actions?.showMovieDetails(pages.movies[index])
+    }
+}
+
+private extension Array where Element == MoviesPage {
+    var movies: [Movie] { flatMap { $0.movies } }
 }
---
+++ Telda-MoviesApp/Presentations/Utils/Extensions.swift
@@ -0,0 +1,6 @@
+//
+//  Extensions.swift
+//  Telda-MoviesApp
+//
+//  Created by Khaled Elshamy on 27/06/2025.
+//
